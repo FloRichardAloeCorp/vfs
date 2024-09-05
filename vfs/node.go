b99c6e2223c9f3acff80e32ef8d38a2fad8dbe05
Florian Richard <florian.richard@aloe-corp.com>
2024-09-05 09:48:35 +0200

    Add CreatedAt and LastUpdate field on node

--- vfs/node.go
+++ vfs/node.go
@@ -1,6 +1,10 @@
 package vfs
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type NodeType int
 
@@ -21,28 +25,36 @@ func (t *NodeType) String() string {
 }
 
 type Node struct {
-	ID       uuid.UUID
-	Name     string
-	Type     NodeType
-	Content  []byte
-	Children map[string]*Node
+	ID         uuid.UUID
+	Name       string
+	Type       NodeType
+	Content    []byte
+	Children   map[string]*Node
+	CreatedAt  time.Time
+	LastUpdate time.Time
 }
 
 func NewFileNode(name string, content []byte) *Node {
+	now := time.Now().UTC()
 	return &Node{
-		ID:      uuid.New(),
-		Name:    name,
-		Type:    File,
-		Content: content,
+		ID:         uuid.New(),
+		Name:       name,
+		Type:       File,
+		Content:    content,
+		CreatedAt:  now,
+		LastUpdate: now,
 	}
 }
 
 func NewDirectoryNode(name string) *Node {
+	now := time.Now().UTC()
 	return &Node{
-		ID:       uuid.New(),
-		Name:     name,
-		Type:     Directory,
-		Children: make(map[string]*Node),
+		ID:         uuid.New(),
+		Name:       name,
+		Type:       Directory,
+		Children:   make(map[string]*Node),
+		CreatedAt:  now,
+		LastUpdate: now,
 	}
 }
 
